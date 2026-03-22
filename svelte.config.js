7985263ab98c068194b933e713f6ac33a226f11e
_ProtonPLUS <mcib088@aucklanduni.ac.nz>
2026-03-22 14:05:19 +1300

    Github workflows

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,11 +8,20 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see docs
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
+			}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : '/Portfolio-Website', // Change to your repo name
+		}
+ 	},
 };
+
 
 export default config;
